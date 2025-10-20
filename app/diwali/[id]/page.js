cbfcca02fa928559e486c1290b361a58d0d7e9af
aryansingh-0 <aryan03aryansingh@gmail.com>
2025-10-20 11:28:37 +0530

    Udpaete

--- app/diwali/[id]/page.js
+++ app/diwali/[id]/page.js
@@ -153,14 +153,14 @@ export default async function Page({ params }) {
       <LightDeepak />
 
       {/* Wish Card */}
-      <div className="relative w-full   md:max-w-[80%] md:bg-white/90 md:backdrop-blur-xl md:shadow-2xl rounded-3xl px-2 py-10 text-center md:border border-orange-200">
+      <div className="relative w-full   md:max-w-[60%] md:bg-white/90 md:backdrop-blur-xl md:shadow-2xl rounded-3xl px-2 py-10 text-center md:border border-orange-200">
         {/* Title */}
         <h1 className="text-4xl capitalize font-extrabold text-orange-700 mb-6">
           {wish.name} ko Diwali Wish
         </h1>
-
-        {/* Wish Image */}
-        <div className="h-52 mb-3 rounded-2xl w-full overflow-hidden">
+<div className="md:flex md:gap-3">
+   {/* Wish Image */}
+        <div className="h-52 md:h-full mb-3 rounded-2xl  w-full overflow-hidden">
           <Image
             src={`/diwali/${wish.type}.png`}
             alt={`${wish.type} Diwali illustration`}
@@ -176,21 +176,24 @@ export default async function Page({ params }) {
           {formattedWishParts}
         </div>
 
+  </div>
+       
         {/* Wisher Info */}
         <p className="text-gray-600 italic mb-6 text-lg text-end">
           By - {wish.wisher}
         </p>
-      </div>
-
-      {/* Button */}
-      <div className="mt-3 flex justify-center">
+         <div className="mt-3 flex justify-center">
         <Link
           href="/diwali"
           className="inline-flex items-center gap-3 px-8 py-4 rounded-full text-lg font-bold text-white bg-gradient-to-r from-orange-500 to-yellow-400 shadow-lg hover:shadow-xl hover:scale-105 transition-all duration-300 animate-pulse"
         >
-          🪔 Create a wish for your friend
+          Create a wish for your friend
         </Link>
       </div>
+      </div>
+
+      {/* Button */}
+     
      
             <MediumRectangle/>
              
